efac08427c12a0ca557be345abc10d4ce2606d2c
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-09-09 21:52:52 -0300

    Add files via upload

---
+++ R/get_url.R
@@ -0,0 +1,11 @@
+#' URL da SSP-SP
+#' Recebe uma URL
+#'
+#' @param x de uma url
+#'
+#' @export
+get_url <- function(x){
+  "http://www.ssp.sp.gov.br/Estatistica/ViolenciaMulher.aspx"
+}
+
+get_url(url)
